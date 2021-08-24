467719405f7c330794bfc054d23784ec7bc45c2e
chaudhariyash10 <chaudhariyash10@gmail.com>
2021-08-24 12:57:07 +0530

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const hats = () => {
+  return (
+    <h1>Hats</h1>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component = {HomePage}/>
+        <Route path="/hats" component = {hats}/>
+      </Switch>
     </div>
   );
 }
