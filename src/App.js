7871bad181dbdee1dd6381cf65c7e59dcc03a7bc
chaudhariyash10 <chaudhariyash10@gmail.com>
2022-03-03 20:07:17 +0530

    Updating app to redirect to homepage if sign in already

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setCurrentUser} from "./redux/user/users.actions"
 
@@ -44,14 +44,17 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component = {HomePage}/>
         <Route path="/shop" component={ShopPage}/>
-        <Route path="/signin" component={SignInAndSignUpPage}/>
+          <Route exact path="/signin" render={() => this.props.currentUser ?( <Redirect to="/"/>) : (<SignInAndSignUpPage/>)}/>
       </Switch>
     </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
